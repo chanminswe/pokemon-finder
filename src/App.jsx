bce8f52e3b4337670dc0d7adc045a2928c37a862
chanminswe <chanswe67@gmail.com>
2024-12-05 19:38:34 +0630

    added fetch to get pokemon api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
+  const [pokemon, setPokemon] = useState("");
+
+  function searchPokemon() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+  }
+
   return (
     <div className="pokemon-main-container">
       <header className="search-pokemon-container">
-        <input className="search-input" />
-        <FontAwesomeIcon  className="search-icon"icon={faMagnifyingGlass} />
+        <input
+          onChange={(event) => setPokemon(event.target.value)}
+          className="search-input"
+        />
+        <FontAwesomeIcon
+          onClick={searchPokemon}
+          className="search-icon"
+          icon={faMagnifyingGlass}
+        />
       </header>
     </div>
   );
