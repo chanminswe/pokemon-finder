596b84d21b0849b4ed9511673de6db7330b113bd
chanminswe <chanswe67@gmail.com>
2024-12-05 19:11:18 +0630

    styling so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './App.css';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='pokemon-main-container'>
+      <header className='search-pokemon-container'>
+        <input className='search-input'/>
+      </header>
+    </div>
   )
 }
 
