bdc17e4a1babcbbb502a699a18bf16de01cb2a1c
chanminswe <chanswe67@gmail.com>
2024-12-05 19:26:02 +0630

    added search icon from font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
   return (
-    <div className='pokemon-main-container'>
-      <header className='search-pokemon-container'>
-        <input className='search-input'/>
+    <div className="pokemon-main-container">
+      <header className="search-pokemon-container">
+        <input className="search-input" />
+        <FontAwesomeIcon  className="search-icon"icon={faMagnifyingGlass} />
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
