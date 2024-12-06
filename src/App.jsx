2e918129c98637b12a3fa2d3e45cf687a9d02b93
chanminswe <chanswe67@gmail.com>
2024-12-06 12:59:45 +0630

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -6,6 +6,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 const App = () => {
   const [pokemon, setPokemon] = useState("");
   const [pokeapi, setPokeapi] = useState();
+  const [pokemonStats, setPokemonStats] = useState([]);
 
   async function searchPokemon() {
     await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -18,6 +19,7 @@ const App = () => {
       .then((data) => {
         console.log(data);
         setPokeapi(data);
+        setPokemonStats(data.stats);
       })
       .catch((error) => {
         console.error(error);
@@ -37,17 +39,38 @@ const App = () => {
           icon={faMagnifyingGlass}
         />
       </header>
-      {pokeapi && (
-        <section className="image-section">
-          <img className="pokemon-image" src={pokeapi.sprites.front_default} alt={pokeapi.name} />
-          {pokeapi.types.length === 1 ? (
-            <div>{pokeapi.types[0].type.name}</div>
-          ) : (
-            <section className="type-section">
-              <div className="type-style">{pokeapi.types[0].type.name}</div>
-              <div className="type-style">{pokeapi.types[1].type.name}</div>
-            </section>
-          )}
+      <>
+        {pokeapi && (
+          <section className="image-section">
+            <img
+              className="pokemon-image"
+              src={pokeapi.sprites.front_default}
+              alt={pokeapi.name}
+            />
+            {pokeapi.types.length === 1 ? (
+              <div>{pokeapi.types[0].type.name}</div>
+            ) : (
+              <section className="type-section">
+                <div className="type-style">{pokeapi.types[0].type.name}</div>
+                <div className="type-style">{pokeapi.types[1].type.name}</div>
+              </section>
+            )}
+          </section>
+        )}
+      </>
+      {pokemonStats.length > 0 && (
+        <section className="stats-section">
+          {pokemonStats.map((stats, index) => (
+            <>
+              <div className="stats-container" key={index}>
+                <div className="stats-name-container">
+                  <p>{stats.stat.name}</p>
+                  <p>{stats.base_stat}</p>
+                </div>
+                <progress value={stats.base_stat} max={255}></progress>
+              </div>
+            </>
+          ))}
         </section>
       )}
     </div>
